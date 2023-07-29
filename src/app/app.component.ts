d97218678c529d970db0f8fd0686830ccef33829
Tobias Warneke <t.warneke@gmx.net>
2023-07-30 00:15:31 +0200

    corrected solutions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,9 @@ export class AppComponent {
   math_exercises : Exercise[] = [
     {topic: "Vereinfache", text:"$x + 6a + 5x + 8a$", choice:[ "$5x^2 + 14a$", "$6x - 2a$", "$6x + 14a$" ], solution:2 },
     {topic: "Vereinfache", text:"$7x + 6a - 2b - 8a - 7x$", choice:[ "$- 2a - 2b$", "$- 2a - 9x$", "$- 2a - 2b - 7x$" ], solution:0 },
-    {topic: "Vereinfache", text:"$3x + 6a - 2x - 8a$", choice:[ "$x - 2a$", "$5x - 2a$", "$x + 2a$" ], solution:2 },
+    {topic: "Vereinfache", text:"$3x + 6a - 2x - 8a$", choice:[ "$x - 2a$", "$5x - 2a$", "$x + 2a$" ], solution:0 },
     {topic: "Vereinfache", text:"$\\frac{3}{4} x + \\frac{2}{3} a - 2x - \\frac{1}{3} a$", choice:[ 
-        "$\\frac{5}{4} x + a - 2x$", "$-\\frac{5}{4} x + a - 2x$", "$-\\frac{5}{4} x + \\frac{1}{3} a - 2x$" ], solution:2 }
+        "$\\frac{5}{4} x + a$", "$-\\frac{5}{4} x + a$", "$-\\frac{5}{4} x + \\frac{1}{3} a$" ], solution:2 }
   ];
 
   /* math_exercises Exercise[] = []
