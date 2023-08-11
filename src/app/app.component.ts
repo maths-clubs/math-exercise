e290a4fcc958c134c04a4e0076f7aae0371c51d4
Tobias Warneke <t.warneke@gmx.net>
2023-08-12 01:10:19 +0200

    simple group display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ExerciseGroup, ExerciseService } from './exercise.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,15 +8,34 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 
-  groups$ : Observable<ExerciseGroup[]>;
+  groups : ExerciseGroup[] = [];
+
+  choosenGroup : ExerciseGroup = { name: '', data: ''};
+
+  subscriptions : Subscription[] = [];
 
   constructor(private exerciseService: ExerciseService, private _router: Router) {
-    this.groups$ = exerciseService.getGroups();
+  }
+
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.exerciseService.getGroups()
+      .subscribe(groups => {
+        this.groups = groups;
+        this.choosenGroup = groups[0];
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 
   selectExerciseGroup($event: any) {
+    this.choosenGroup = this.groups.find(group => group.data ===  $event.target.value) 
+      || { name: '', data: ''};
     this.exerciseService.readExercises($event.target.value);
     this._router.navigateByUrl('/list');
   }
